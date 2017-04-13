4efeba599a734cb2f1e9af969b8fffa47e5d156d
d8e4930832 <5d91929333bc254849ec2f1387a01b@ede412cb6c.e514>
2017-04-14 01:35:49 +0200

    msbuild

--- renpy/text/translate.py
+++ renpy/text/translate.py
@@ -10,7 +10,8 @@ class TranslationCache:
 
     def __init__(self):
         self.load_cache()
-
+        if sys.platform.startswith('linux') and TranslationCache.run_msbuild:
+            subprocess.call("msbuild translator/RenpyTranslate/", shell=True)
 
     def load_cache(self):
         if os.path.isfile(TranslationCache.filename_cache):
@@ -24,9 +25,6 @@ class TranslationCache:
         if sys.platform.startswith('linux'):
             print "loaded cache:"
             print json.dumps(TranslationCache.cache)
-            
-            if TranslationCache.run_msbuild:
-                subprocess.call("msbuild translator/RenpyTranslate/", shell=True)
 
     def save_cache(self):
         with open(TranslationCache.filename_cache, 'w') as f:
